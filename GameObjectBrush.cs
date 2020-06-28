ef2f6f54c47b29322fcc2606b977ff06fd0e182e
Raphael Alexander Priatama <37859544+Developer-The-Great@users.noreply.github.com>
2020-06-28 20:28:38 +0200

    Committed un-commented files

---
+++ GameObjectBrush.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteAlways]
+public class GameObjectBrush : MonoBehaviour
+{
+    [Range(0.0f, 20.0f),SerializeField,Tooltip("The radius of the placement circle")]
+    private float radius;
+
+    [Range(0.0f, 10.0f), SerializeField, 
+        Tooltip(" The minimum allowed distance for each placed object to another placed object")]
+    public float radiusFromOtherObjects;
+
+    public GameObject objectToPlace;
+    public Transform parentToPlaceIn;
+
+    [Range(0.0f, 1.0f)]
+    public float density;
+
+    private List<GameObject> lastPlacedGameObjects = new List<GameObject>();
+
+    public Terrain Terrain;
+
+    public bool alwaysPlaceAtLeastOne = true;
+
+    public bool randomlyRotateObjects = true;
+
+    
+
+    public float Radius
+    {
+        get
+        {
+            return radius;
+        }
+        set
+        {
+            Debug.Log("set");
+            radius = Mathf.Clamp(value,0.05f,20.0f);
+        }
+    }
+
+
+    private void Start()
+    {
+        Radius = 1.0f;
+    }
+    
+
+    public void PlaceObjectsAt(Vector3 position)
+    {
+        lastPlacedGameObjects.Clear();
+
+        //ensure radius > distanceFromOtherObjects
+
+        if (radiusFromOtherObjects > radius)
+        {
+            Debug.LogError("radiusFromOtherObjects CANNOT be bigger than radius");
+            return;
+        }
+
+        float spaceForOtherCircles = radius - radiusFromOtherObjects;
+        float diameterFromOtherObjects = radiusFromOtherObjects * 2;
+
+        // number of circles that can be placed excluding center
+        int circlePlacementCount = (int)(spaceForOtherCircles/diameterFromOtherObjects);
+        Debug.Log(" circlePlacementCount " + circlePlacementCount);
+
+        //get absolute value of radius - radiusFromOtherObjects + (2*radiusFromOtherObjects)
+        float leftoverSpace = spaceForOtherCircles - circlePlacementCount * diameterFromOtherObjects;
+
+        //Instantiate Object on center
+        if(alwaysPlaceAtLeastOne)
+        {
+            PlaceObject(position);
+        }
+        else
+        {
+            if(DoCheckIfShouldPlace()) { PlaceObject(position); }
+        }
+        
+
+        // get minimum chord length to avoid collision
+        float minimumChordLength = diameterFromOtherObjects;
+
+        float placementCircleDiameter = diameterFromOtherObjects*2;
+
+        //for CirclePlacementCount
+        for (int i = 0; i < circlePlacementCount; i++)
+        {
+            Vector3 nextSpawnPoint = Vector3.forward * diameterFromOtherObjects * (i+1);
+
+            float minimumAngleToAvoidCollision =
+                2 * Mathf.Asin(minimumChordLength / (placementCircleDiameter)) * Mathf.Rad2Deg;
+
+            int numberOfObjectsPlacableInCircle = (int)(361.0f / minimumAngleToAvoidCollision);
+            Debug.Log(" numberOfObjectsPlacableInCircle " + numberOfObjectsPlacableInCircle);
+
+            float leftoverAngle = Mathf.Abs(360.0f - numberOfObjectsPlacableInCircle * minimumAngleToAvoidCollision);
+            Debug.Log("leftoverAngle  " + leftoverAngle);
+
+            for (int j = 0; j < numberOfObjectsPlacableInCircle; j++)
+            {
+                float addedAngleInIteration = Random.Range(0.0f, leftoverAngle); 
+                leftoverAngle -= addedAngleInIteration;
+
+                nextSpawnPoint = Quaternion.AngleAxis(minimumAngleToAvoidCollision + addedAngleInIteration, Vector3.up) * nextSpawnPoint;
+
+                Vector3 curentSpawnPoint = position + nextSpawnPoint;
+
+                if(DoCheckIfShouldPlace())
+                {
+                    PlaceObject(curentSpawnPoint);
+                }
+            }
+
+            placementCircleDiameter += diameterFromOtherObjects;
+        }
+    }
+
+
+
+    private bool DoCheckIfShouldPlace()
+    {
+        return Random.Range(0, 1.0f) < density;
+    }
+
+
+    private void PlaceObject(Vector3 position)
+    {
+        GameObject placedObject = Instantiate(objectToPlace, position, Quaternion.identity);
+        lastPlacedGameObjects.Add(placedObject);
+
+        if (Terrain != null)
+        {
+            ModifyYBasedOnTerrain(placedObject);
+            ModifyLookRotationBasedOnTerrain(placedObject);
+        }
+
+        if(parentToPlaceIn != null)
+        {
+            placedObject.transform.SetParent(parentToPlaceIn);
+        }
+
+        if(randomlyRotateObjects)
+        {
+            placedObject.transform.rotation *= Quaternion.AngleAxis(Random.Range(0, 360), placedObject.transform.up);
+        }
+    }
+
+    public void DeleteObjectsInRange(Vector3 position)
+    {
+        if(parentToPlaceIn ==null)
+        {
+            Debug.LogError("This feature only works if ParentToPlaceIn is set!");
+            return;
+        }
+
+        foreach(Transform child in parentToPlaceIn)
+        {
+            float distanceSquared = Vector3.SqrMagnitude(child.transform.position - position);
+            if(distanceSquared < Mathf.Pow(radius,2))
+            {
+                DestroyImmediate(child.gameObject);
+            }
+        }
+    }
+
+    private void ModifyYBasedOnTerrain(GameObject obj)
+    {
+        float newY = Terrain.SampleHeight(obj.transform.position) + Terrain.GetPosition().y;
+        obj.transform.position = new Vector3(obj.transform.position.x, newY, obj.transform.position.z);
+    }
+
+
+    private void ModifyLookRotationBasedOnTerrain(GameObject obj)
+    {
+        Vector3 objectPosition = obj.transform.position;
+
+        Vector3 nextOnForward = objectPosition + Vector3.forward * 0.01f;
+        float nextOnForwardY = Terrain.SampleHeight(nextOnForward) + Terrain.GetPosition().y;
+        nextOnForward = new Vector3(nextOnForward.x, nextOnForwardY, nextOnForward.z);
+
+        Vector3 nextOnRight = objectPosition + Vector3.right * 0.01f;
+        float nextOnRightY = Terrain.SampleHeight(nextOnRight) + Terrain.GetPosition().y;
+        nextOnRight = new Vector3(nextOnRight.x, nextOnRightY, nextOnRight.z);
+
+        Vector3 forward = nextOnForward - objectPosition;
+        Vector3 right = nextOnRight - objectPosition;
+
+        obj.transform.LookAt(nextOnForward, Vector3.Cross(forward, right).normalized);
+        
+
+    }
+
+    public void DestroyRecentlyPlacedObjects()
+    {
+        foreach(var obj in lastPlacedGameObjects)
+        {
+            DestroyImmediate(obj);
+        }
+    }
+
+}
---
+++ GameObjectBrushEditor.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameObjectBrush))]
+public class GameObjectBrushEditor : Editor
+{
+    GameObjectBrush brush;
+    
+    GameObject actualObject;
+    GameObject sampleObject;
+
+    Vector3 lastHitPoint;
+
+    private static GUIStyle ToggleButtonStyleNormal = null;
+
+    private string destructionModeStr = "Destruction Mode";
+    private string InstantiationModeStr = "Instantiation Mode";
+
+    private string displayString;
+
+    private bool isInstantiationMode = true;
+
+    private float destructionSphereTransparency = 0.8f;
+
+    private void OnEnable()
+    {
+        brush = target as GameObjectBrush;
+
+        displayString = InstantiationModeStr;
+        isInstantiationMode = true;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (ToggleButtonStyleNormal == null)
+        {
+            ToggleButtonStyleNormal = "Button";
+        }
+
+        GUI.backgroundColor = isInstantiationMode ? Color.green : Color.red;
+
+        if(GUILayout.Button(displayString, ToggleButtonStyleNormal))
+        {
+            isInstantiationMode = !isInstantiationMode;
+
+            if(displayString == InstantiationModeStr)
+            {
+                displayString = destructionModeStr;
+            }
+            else
+            {
+                displayString = InstantiationModeStr;
+            }
+        }
+
+
+
+
+        GUI.backgroundColor = Color.white;
+
+        DrawDefaultInspector();
+
+        GUI.backgroundColor = Color.red;
+        if (GUILayout.Button("Delete Previously Placed Objects", ToggleButtonStyleNormal))
+        {
+            brush.DestroyRecentlyPlacedObjects();
+        }
+    }
+
+
+    private void OnSceneGUI()
+    {
+        if (!brush.isActiveAndEnabled) { return; }
+
+        Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+        RaycastHit hit = new RaycastHit();
+
+        if(isInstantiationMode)
+        {
+            HandleCreationModeGUI(ray, hit);
+        }
+        else
+        {
+            HandleDestructionModeGUI(ray, hit);
+        }
+
+        SceneView.RepaintAll();
+    }
+
+    private void HandleDestructionModeGUI(Ray ray, RaycastHit hit)
+    {
+        if (Physics.Raycast(ray, out hit))
+        {
+            lastHitPoint = hit.point;
+            DrawDestructionHandles(hit);
+
+            if (Event.current.type == EventType.MouseDown && Event.current.button == 1)
+            {
+                brush.DeleteObjectsInRange(hit.point);
+            }
+        }
+    }
+
+    private void HandleCreationModeGUI(Ray ray,RaycastHit hit)
+    {
+        
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            lastHitPoint = hit.point;
+            DrawBrushHandles(hit);
+
+            if (Event.current.type == EventType.MouseDown && Event.current.button == 1)
+            {
+                brush.PlaceObjectsAt(hit.point);
+            }
+
+        }
+
+
+        PlacePreviewObject();
+    }
+
+    private void PlacePreviewObject()
+    {
+        if (brush.objectToPlace != null)
+        {
+            if (sampleObject != brush.objectToPlace)
+            {
+                Debug.Log("sampleObject != brush.objectToPlace");
+                if (!NullCheck(actualObject))
+                {
+                    DestroyImmediate(actualObject);
+
+                }
+
+                foreach (Transform child in brush.transform)
+                {
+                    DestroyImmediate(child.gameObject);
+                }
+
+                sampleObject = brush.objectToPlace;
+                actualObject = Instantiate(brush.objectToPlace, lastHitPoint, Quaternion.identity);
+                actualObject.name = "actualObject";
+                actualObject.transform.SetParent(brush.transform);
+            }
+        }
+
+        if (actualObject != null)
+        {
+            actualObject.transform.position = lastHitPoint;
+        }
+    }
+
+    private void DrawBrushHandles(RaycastHit hit)
+    {
+        Quaternion circleRotation = Quaternion.LookRotation(hit.normal);
+
+        Handles.color = Color.white;
+        Handles.CircleHandleCap(0, hit.point, circleRotation,
+            brush.Radius, EventType.Repaint);
+
+        Handles.color = Color.red;
+        Handles.CircleHandleCap(0, hit.point, circleRotation,
+            brush.radiusFromOtherObjects, EventType.Repaint);
+    }
+
+    private void DrawDestructionHandles(RaycastHit hit)
+    {
+        Quaternion circleRotation = Quaternion.LookRotation(hit.normal);
+
+        Color destructionSphereColor = Color.red;
+
+        destructionSphereColor.a = destructionSphereTransparency;
+        Handles.color = destructionSphereColor;
+
+        Handles.CircleHandleCap(0, hit.point, circleRotation,
+            brush.Radius * 0.1f, EventType.Repaint);
+
+        Handles.color = Color.red;
+        Handles.CircleHandleCap(0, hit.point, circleRotation,
+            brush.Radius, EventType.Repaint);
+
+        Handles.CircleHandleCap(0, hit.point, circleRotation,
+            brush.Radius * 0.95f, EventType.Repaint);
+    }
+
+    private bool NullCheck(GameObject obj)
+    {
+        return obj == null;
+    }
+    
+    
+
+}
